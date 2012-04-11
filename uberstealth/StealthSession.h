7bef581df48355de01d1e72a201e12a0f2692d29
Jan Newger <jan.newger@newgre.net>
2012-04-12 01:01:04 +0200

    - Code cleanup part 4.

--- uberstealth/StealthSession.h
+++ uberstealth/StealthSession.h
@@ -40,13 +40,13 @@ template <typename EngineT>
 class StealthSession {
 public:
 	StealthSession(const std::string& profilePath):
-		hProcess_(INVALID_HANDLE_VALUE),
-		profilePath_(profilePath),
-		currentProfile_(HideDebuggerProfile::readProfileByName(profilePath)) {}
+		_hProcess(INVALID_HANDLE_VALUE),
+		_profilePath(profilePath),
+		_currentProfile(HideDebuggerProfile::readProfileByName(profilePath)) {}
 	virtual ~StealthSession() {}
 	
 	virtual void handleDbgAttach(unsigned int processId) {
-		if (currentProfile_.getEnableDbgAttachEnabled()) {
+		if (_currentProfile.getEnableDbgAttachEnabled()) {
 			// We need to start dll injection in a background thread because the dll injection
 			// will block until the debuggee is resumed.
 			boost::thread(boost::bind(&StealthSession::dbgAttachThread, this, processId));
@@ -54,7 +54,7 @@ public:
 	}
 
 	virtual void handleProcessStart(unsigned int processId, uintptr_t baseAddress) {
-		if (currentProfile_.getEnableDbgStartEnabled()) {
+		if (_currentProfile.getEnableDbgStartEnabled()) {
 			// TODO: the exception should NOT be catched here; instead it should bubble up to the caller which then needs to handle it!
 			try	{
 				injectionBeacon_ = boost::make_shared<InjectionBeacon>(processId);
@@ -67,23 +67,23 @@ public:
 				// TODO: move functionality of "remove" into destructor.
 				if (ipc_) ipc_->remove();
 				ipc_ = boost::make_shared<uberstealth::IPCConfigExchangeWriter>(processId);
-				ipc_->setProfileFile(profilePath_);
+				ipc_->setProfileFile(_profilePath);
 				ipc_->setIPCPEHeaderData(uberstealth::IPCPEHeaderData(baseAddress, iatMod.readNTHeaders()));
 				ipc_->setPERestoreRequired(true);
 				iatMod.writeIAT(getStealthDllPath());
 			}
 			catch (const std::exception& e) {
-				engine_.logString("Failed to inject stealth dll (%s): %s.\n", getStealthDllPath().c_str(), e.what());
+				_engine.logString("Failed to inject stealth dll (%s): %s.\n", getStealthDllPath().c_str(), e.what());
 			}
 			catch (...) {
-				engine_.logString("Unknown error while trying to inject stealth dll (%s).\n", getStealthDllPath().c_str());
+				_engine.logString("Unknown error while trying to inject stealth dll (%s).\n", getStealthDllPath().c_str());
 			}
 		}
 	}
 
 	virtual void handleProcessExit() {
 		stopDrivers();
-		CloseHandle(hProcess_);
+		CloseHandle(_hProcess);
 	}
 
 	virtual void handleBreakPoint(unsigned int threadID, uintptr_t address) =0;
@@ -95,7 +95,7 @@ protected:
 	virtual std::string getStealthDllPath() =0;
 
 	void handleRtlGetNtGlobalFlags(unsigned int processID) {
-		if (!currentProfile_.getRtlGetNtGlobalFlagsEnabled()) return;
+		if (!_currentProfile.getRtlGetNtGlobalFlagsEnabled()) return;
 
 		// assume that ntdll is loaded to the same IBA across processes on ASLR systems
 		HMODULE hNtDll = LoadLibrary(L"ntdll.dll");
@@ -108,7 +108,7 @@ protected:
 				process.writeMemory(address, opcodes, 3);
 			}
 			catch (const MemoryAccessException& e){
-				engine_.logString("Error while trying to patch RtlGetNtGlobalFlags: %s\n", e.what());
+				_engine.logString("Error while trying to patch RtlGetNtGlobalFlags: %s\n", e.what());
 			}
 		}
 		FreeLibrary(hNtDll);
@@ -121,68 +121,68 @@ protected:
 
 	void startDrivers() {
 		try {		
-			if (currentProfile_.getRDTSCDriverLoad()) {
+			if (_currentProfile.getRDTSCDriverLoad()) {
 				ResourceItem drvResource = getRDTSCDriverResource();
-				std::string rdtscName = currentProfile_.getRDTSCDriverCustomName();
+				std::string rdtscName = _currentProfile.getRDTSCDriverCustomName();
 				rdtscDriver_.startDriver(drvResource, rdtscName);
-				RDTSCMode mode = currentProfile_.getRDTSCDriverMode();
-				unsigned int param = mode == constant ? 0 : currentProfile_.getRDTSCDriverDelta();
+				RDTSCMode mode = _currentProfile.getRDTSCDriverMode();
+				unsigned int param = mode == constant ? 0 : _currentProfile.getRDTSCDriverDelta();
 				DWORD ioctlCode = mode == constant ? (DWORD)IOCTL_RDTSCEMU_METHOD_ALWAYS_CONST : (DWORD)IOCTL_RDTSCEMU_METHOD_INCREASING;
 				rdtscDriver_.setMode(ioctlCode, &param, sizeof(param));
-				engine_.logString("Successfully started RDTSC emulation driver from %s\n", rdtscDriver_.getDriverPath().c_str());
+				_engine.logString("Successfully started RDTSC emulation driver from %s\n", rdtscDriver_.getDriverPath().c_str());
 			}
 		} catch (const std::exception& e) {
-			engine_.logString("Error while trying to load RDTSC driver: %s\n", e.what());
+			_engine.logString("Error while trying to load RDTSC driver: %s\n", e.what());
 		}
 
 		try	{
-			if (currentProfile_.getStealthDriverLoad()) {
+			if (_currentProfile.getStealthDriverLoad()) {
 				ResourceItem stealthDrvResource = getStealthDriverResource();
-				std::string stealthName = currentProfile_.getStealthDriverCustomName();
+				std::string stealthName = _currentProfile.getStealthDriverCustomName();
 				stealthDriver_.startDriver(stealthDrvResource, stealthName);
-				engine_.logString("Successfully started stealth driver from %s\n", stealthDriver_.getDriverPath().c_str());
+				_engine.logString("Successfully started stealth driver from %s\n", stealthDriver_.getDriverPath().c_str());
 
 				StealthHook mode;
-				if (currentProfile_.getStealthDriverNtSetInformationThread()) {
+				if (_currentProfile.getStealthDriverNtSetInformationThread()) {
 					mode = SH_NtSetInformationThread;
 					stealthDriver_.setMode(IOCTL_STEALTHDRIVER_ENABLE_HOOKS, &mode, sizeof(StealthHook));
 				}
-				if (currentProfile_.getStealthDriverNtQueryInformationProcess()) {
+				if (_currentProfile.getStealthDriverNtQueryInformationProcess()) {
 					mode = SH_NtQueryInformationProcess;
 					stealthDriver_.setMode(IOCTL_STEALTHDRIVER_ENABLE_HOOKS, &mode, sizeof(StealthHook));
 				}
 			}
 		}
 		catch (const std::exception& e) {
-			engine_.logString("Error while trying to load stealth driver: %s\n", e.what());
+			_engine.logString("Error while trying to load stealth driver: %s\n", e.what());
 		}
 	}
 
 	void stopDrivers() {
-		if (currentProfile_.getRDTSCDriverUnload()) {
+		if (_currentProfile.getRDTSCDriverUnload()) {
 			try {
 				bool wasRunning = rdtscDriver_.isRunning();
 				rdtscDriver_.stopDriver();
-				if (wasRunning) engine_.logString("Successfully unloaded RDTSC emulation driver");
+				if (wasRunning) _engine.logString("Successfully unloaded RDTSC emulation driver");
 			} catch (const std::exception& e) {
-				engine_.logString("Error while trying to stop RDTSC driver: %s\n", e.what());
+				_engine.logString("Error while trying to stop RDTSC driver: %s\n", e.what());
 			}
 		}
 
-		if (currentProfile_.getStealthDriverUnload()) {
+		if (_currentProfile.getStealthDriverUnload()) {
 			try	{
 				bool wasRunning = stealthDriver_.isRunning();
 				stealthDriver_.stopDriver();
-				if (wasRunning) engine_.logString("Successfully unloaded stealth driver\n");
+				if (wasRunning) _engine.logString("Successfully unloaded stealth driver\n");
 			} catch (const std::exception& e) {
-				engine_.logString("Error while trying to stop stealth driver: %s\n", e.what());
+				_engine.logString("Error while trying to stop stealth driver: %s\n", e.what());
 			}
 		}
 	}
 
-	HANDLE getProcessHandle() const { return hProcess_; };
-	EngineT engine_;
-	HideDebuggerProfile currentProfile_;
+	HANDLE getProcessHandle() const { return _hProcess; };
+	EngineT _engine;
+	HideDebuggerProfile _currentProfile;
 
 private:
 	void dbgAttachThread(unsigned int processId) {
@@ -191,29 +191,29 @@ private:
 			performCommonInit(processId);
 			if (ipc_) ipc_->remove();
 			ipc_ = boost::make_shared<uberstealth::IPCConfigExchangeWriter>(processId);
-			ipc_->setProfileFile(profilePath_);
+			ipc_->setProfileFile(_profilePath);
 			ipc_->setPERestoreRequired(false);
 
 			Process process(processId);
 			InjectLibrary injector(getStealthDllPath(), process);
 			if (!injector.injectLib())
-				engine_.logString("Injection of stealth dll failed (while attaching to process)\n");
+				_engine.logString("Injection of stealth dll failed (while attaching to process)\n");
 		} catch (const std::exception& e) {
-			engine_.logString("Error while trying to attach to process: %s\n", e.what());
+			_engine.logString("Error while trying to attach to process: %s\n", e.what());
 		}
 	}
 
 	void acquireProcessHandle(unsigned int processID) {
-		hProcess_ = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-		if (hProcess_ == INVALID_HANDLE_VALUE) throw std::runtime_error("Unable to obtain process handle from debuggee");
+		_hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+		if (_hProcess == INVALID_HANDLE_VALUE) throw std::runtime_error("Unable to obtain process handle from debuggee");
 	}
 
 	boost::shared_ptr<uberstealth::IPCConfigExchangeWriter> ipc_;
 	DriverControl rdtscDriver_;
 	DriverControl stealthDriver_;
-	HANDLE hProcess_;
+	HANDLE _hProcess;
 	boost::shared_ptr<InjectionBeacon> injectionBeacon_;
-	std::string profilePath_;
+	std::string _profilePath;
 };
 
 }
